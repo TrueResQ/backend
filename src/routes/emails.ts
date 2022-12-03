6b0a2c86c5b88cac1235a829be5dbfc7dfa35533
Swaraj Phadtare <1swarajphadtare@gmail.com>
2022-12-04 02:01:57 +0530

    Cleanup

--- src/routes/emails.ts
+++ src/routes/emails.ts
@@ -57,7 +57,7 @@ router.post(
 		<meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>*|MC:SUBJECT|*</title>
+		<title>TrueResQ Guardian Request</title>
         
     <style type="text/css">
 		p{
@@ -532,9 +532,6 @@ router.post(
 
 }</style></head>
     <body>
-		<!--*|IF:MC_PREVIEW_TEXT|*-->
-		<!--[if !gte mso 9]><!----><span class="mcnPreviewText" style="display:none; font-size:0px; line-height:0px; max-height:0px; max-width:0px; opacity:0; overflow:hidden; visibility:hidden; mso-hide:all;">*|MC_PREVIEW_TEXT|*</span><!--<![endif]-->
-		<!--*|END:IF|*-->
         <center>
             <table align="center" border="0" cellpadding="0" cellspacing="0" height="100%" width="100%" id="bodyTable">
                 <tr>
